opencog/opencog

Change merge function signature

opencog/atomspace/CountTruthValue.cc
@@ -96,7 +96,7 @@ bool CountTruthValue::operator==(const TruthValue& rhs) const
 // Note: this is NOT the merge formula used by PLN.  This is
 // because the CountTruthValue usally stores an integer count,
 // and a log-probability or entropy, instead of a confidence.
-TruthValuePtr CountTruthValue::merge(TruthValuePtr other) const
+TruthValuePtr CountTruthValue::merge(TruthValuePtr other,MergeOption mo/*=DEFAULT*/) const
 {
     CountTruthValuePtr oc =
         std::dynamic_pointer_cast<CountTruthValue>(other);

opencog/atomspace/CountTruthValue.h
@@ -61,7 +61,7 @@ public:
     count_t getCount() const;
     confidence_t getConfidence() const;
 
-    virtual TruthValuePtr merge(TruthValuePtr) const;
+    virtual TruthValuePtr merge(TruthValuePtr,MergeOption mo=DEFAULT) const;
 
     static TruthValuePtr createTV(strength_t s, confidence_t f, count_t c)
     {

opencog/atomspace/FuzzyTruthValue.cc
@@ -72,7 +72,7 @@ confidence_t FuzzyTruthValue::getConfidence() const
 }
 
 // This is the merge formula appropriate for PLN.
-TruthValuePtr FuzzyTruthValue::merge(TruthValuePtr other) const
+TruthValuePtr FuzzyTruthValue::merge(TruthValuePtr other,MergeOption mo /*=DEFAULT*/) const
 {
     if (other->getType() != SIMPLE_TRUTH_VALUE) {
         throw RuntimeException(TRACE_INFO,

opencog/atomspace/FuzzyTruthValue.h
@@ -84,7 +84,7 @@ public:
      * the resulting TV is either tv1 or tv2, the result being the one
      * with the highest confidence.
      */
-    TruthValuePtr merge(TruthValuePtr) const;
+    TruthValuePtr merge(TruthValuePtr,MergeOption mo=DEFAULT) const;
 
     static FuzzyTruthValuePtr createSTV(strength_t mean, count_t count)
     {

opencog/atomspace/IndefiniteTruthValue.h
@@ -114,7 +114,7 @@ public:
     strength_t getL_() const;
     bool isSymmetric() const;
 
-    TruthValuePtr merge(TruthValuePtr) const;
+    TruthValuePtr merge(TruthValuePtr,MergeOption mo=DEFAULT) const;
 
     std::string toString() const;
 

opencog/atomspace/NullTruthValue.cc
@@ -62,7 +62,7 @@ confidence_t NullTruthValue::getConfidence() const throw (RuntimeException)
     throw RuntimeException(TRACE_INFO, "Cannot call getConfidence() method of a NullTruthvalue");
 }
 
-TruthValuePtr NullTruthValue::merge(TruthValuePtr) const throw (RuntimeException)
+TruthValuePtr NullTruthValue::merge(TruthValuePtr,MergeOption mo/*=DEFAULT*/) const throw (RuntimeException)
 {
     throw RuntimeException(TRACE_INFO, "Cannot call merge() method of a NullTruthvalue");
 }

opencog/atomspace/NullTruthValue.h
@@ -51,7 +51,7 @@ public:
     virtual bool operator==(const TruthValue& rhs) const;
 
 protected:
-    TruthValuePtr merge(TruthValuePtr) const throw (RuntimeException);
+    TruthValuePtr merge(TruthValuePtr,MergeOption mo=DEFAULT) const throw (RuntimeException);
 };
 
 /** @}*/

opencog/atomspace/SimpleTruthValue.cc
@@ -72,7 +72,7 @@ confidence_t SimpleTruthValue::getConfidence() const
 }
 
 // This is the merge formula appropriate for PLN.
-TruthValuePtr SimpleTruthValue::merge(TruthValuePtr other) const
+TruthValuePtr SimpleTruthValue::merge(TruthValuePtr other,MergeOption mo/*=DEFAULT*/) const
 {
     if (other->getType() != SIMPLE_TRUTH_VALUE) {
         throw RuntimeException(TRACE_INFO,

opencog/pln

opencog/atomspace/CountTruthValue.cc
@@ -101,7 +101,7 @@ TruthValueType CountTruthValue::getType() const
 // Note: this is NOT the merge formula used by PLN.  This is
 // because the CountTruthValue usally stores an integer count,
 // and a log-probability or entropy, instead of a confidence.
-TruthValuePtr CountTruthValue::merge(TruthValuePtr other) const
+TruthValuePtr CountTruthValue::merge(TruthValuePtr other,TVMergeStyle ms/*=DEFAULT*/) const
 {
     CountTruthValuePtr oc =
         std::dynamic_pointer_cast<CountTruthValue>(other);

opencog/atomspace/CountTruthValue.h
@@ -62,7 +62,7 @@ public:
     count_t getCount() const;
     confidence_t getConfidence() const;
 
-    virtual TruthValuePtr merge(TruthValuePtr) const;
+    virtual TruthValuePtr merge(TruthValuePtr,TVMergeStyle ms=DEFAULT) const;
 
     static TruthValuePtr createTV(strength_t s, confidence_t f, count_t c)
     {

opencog/atomspace/FuzzyTruthValue.cc
@@ -72,7 +72,7 @@ confidence_t FuzzyTruthValue::getConfidence() const
 }
 
 // This is the merge formula appropriate for PLN.
-TruthValuePtr FuzzyTruthValue::merge(TruthValuePtr other) const
+TruthValuePtr FuzzyTruthValue::merge(TruthValuePtr other,TVMergeStyle ms/*=DEFAULT*/) const
 {
     if (other->getType() != SIMPLE_TRUTH_VALUE) {
         throw RuntimeException(TRACE_INFO,

opencog/atomspace/FuzzyTruthValue.h
@@ -85,7 +85,7 @@ public:
      * the resulting TV is either tv1 or tv2, the result being the one
      * with the highest confidence.
      */
-    TruthValuePtr merge(TruthValuePtr) const;
+    TruthValuePtr merge(TruthValuePtr,TVMergeStyle ms=DEFAULT) const;
 
     static FuzzyTruthValuePtr createSTV(strength_t mean, count_t count)
     {

opencog/atomspace/IndefiniteTruthValue.h
@@ -114,7 +114,7 @@ public:
     strength_t getL_() const;
     bool isSymmetric() const;
 
-    TruthValuePtr merge(TruthValuePtr) const;
+    TruthValuePtr merge(TruthValuePtr,TVMergeStyle ms=DEFAULT) const;
 
     std::string toString() const;
     TruthValueType getType() const;

opencog/atomspace/NullTruthValue.cc
@@ -67,7 +67,7 @@ TruthValueType NullTruthValue::getType() const
     return NULL_TRUTH_VALUE;
 }
 
-TruthValuePtr NullTruthValue::merge(TruthValuePtr) const throw (RuntimeException)
+TruthValuePtr NullTruthValue::merge(TruthValuePtr,TVMergeStyle ms/*=DEFAULT*/) const throw (RuntimeException)
 {
     throw RuntimeException(TRACE_INFO, "Cannot call merge() method of a NullTruthvalue");
 }

opencog/atomspace/NullTruthValue.h
@@ -52,7 +52,7 @@ public:
     virtual bool operator==(const TruthValue& rhs) const;
 
 protected:
-    TruthValuePtr merge(TruthValuePtr) const throw (RuntimeException);
+    TruthValuePtr merge(TruthValuePtr,TVMergeStyle ms=DEFAULT) const throw (RuntimeException);
 };
 
 /** @}*/

opencog/atomspace/SimpleTruthValue.cc
@@ -72,7 +72,7 @@ confidence_t SimpleTruthValue::getConfidence() const
 }
 
 // This is the merge formula appropriate for PLN.
-TruthValuePtr SimpleTruthValue::merge(TruthValuePtr other) const
+TruthValuePtr SimpleTruthValue::merge(TruthValuePtr other,TVMergeStyle ms/*=DEFAULT*/) const
 {
     if (other->getType() != SIMPLE_TRUTH_VALUE) {
         throw RuntimeException(TRACE_INFO,