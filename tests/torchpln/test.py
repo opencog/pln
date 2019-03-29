89f32003114f22f5b9ed428b107399c79ea95a4d
Anatoly Belikov <awbelikov@gmail.com>
2019-03-29 11:57:01 +0300

    fuzzy conjunction test

--- tests/torchpln/test.py
+++ tests/torchpln/test.py
@@ -5,42 +5,25 @@ from opencog.ure import BackwardChainer
 from opencog.atomspace import AtomSpace, types
 from opencog.utilities import initialize_opencog, finalize_opencog
 from opencog.type_constructors import *
-from cognets import CogModule, CogModel, InputModule, InheritanceModule, get_value, TTruthValue
-from cognets import get_tv, set_tv
+from pln import  TTruthValue
+from pln import get_tv, set_tv
 from pln.rules import propositional
 
-import __main__
-__main__.CogModule = CogModule
-
-
-RED = 0
-GREEN = 1
-BLUE = 2
 
 def init_pln():
     rule_base = ConceptNode('PLN')
     propositional.add_members(rule_base)
     ExecutionLink(SchemaNode("URE:maximum-iterations"),
                   rule_base,
-                  NumberNode('20'))
+                  NumberNode('75'))
     return rule_base
 
-
-class GreenPredicate(CogModule):
-
-    def forward(self, x):
-        """
-        extract green channel and shift it above zero
-        """
-        mean = x.mean(dim=-1).mean(dim=-1)
-        return mean[GREEN]
 
 
 class TestBasic(unittest.TestCase):
 
     def setUp(self):
         self.atomspace = AtomSpace()
-        self.model = CogModel(self.atomspace)
         initialize_opencog(self.atomspace)
         init_pln()
 
@@ -49,11 +32,25 @@ class TestBasic(unittest.TestCase):
         set_tv(ConceptNode("apple"), TTruthValue((0.8, 0.9)))
         inh = InheritanceLink(ConceptNode("apple"),
                 ConceptNode("fruit"))
-        set_tv(inh, TTruthValue((0.8, 0.4)))
+        set_tv(inh, TTruthValue(0.8, 0.4))
         bc = BackwardChainer(self.atomspace, rule_base, ConceptNode("fruit"))
         bc.do_chain()
         result = get_tv(bc.get_results().out[0])
         self.assertTrue(abs(0.68 - float(result.mean)) < 0.0001)
+
+    def test_fuzzy_conjunction(self):
+        rule_base = ConceptNode('PLN')
+        apple = ConceptNode('apple')
+        inh_green = InheritanceLink(PredicateNode("green"), ConceptNode("color"))
+        set_tv(inh_green, torch.tensor([0.96, .9]))
+        ev_green = EvaluationLink(PredicateNode('green'), apple)
+        set_tv(ev_green, TTruthValue(0.85, 0.95))
+        conj = AndLink(ev_green, inh_green)
+        bc = BackwardChainer(self.atomspace, rule_base, conj)
+        bc.do_chain()
+        result = get_tv(bc.get_results().out[0])
+        self.assertTrue(abs(result.mean - 0.85) < 0.0001)
+        self.assertTrue(abs(result.confidence - 0.9) < 0.0001)
 
     def tearDown(self):
         self.atomspace = None
