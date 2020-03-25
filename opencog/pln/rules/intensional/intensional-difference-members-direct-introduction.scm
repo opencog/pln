1bd369929a8fd01d4a1095b0b381dd99c9e0fe06
leungmanhin <manhinleung@gmail.com>
2020-03-25 11:45:44 +0100

    Calculate the TVs

--- opencog/pln/rules/intensional/intensional-difference-members-direct-introduction.scm
+++ opencog/pln/rules/intensional/intensional-difference-members-direct-introduction.scm
@@ -46,6 +46,63 @@
           ;; Premise
           (IntensionalDifference A B))))))
 
+;; Formula
+(define (intensional-difference-members-direct-introduction conclusion premise)
+  ;; Given two concepts A and B, return the attraction link
+  ;;
+  ;; Attraction <TV>
+  ;;   A
+  ;;   B
+  (define (get-attraction A B)
+    (cog-link 'AttractionLink A B))
+
+  ;; Given an attraction link, return the strength which is
+  ;; the product of the mean and the confidence of the TV on
+  ;; on the attraction link
+  ;;
+  ;; Attraction <TV>
+  ;;   A
+  ;;   B
+  (define (get-pattern-strength at)
+      (if (null? at) 0 (* (cog-mean at) (cog-confidence at))))
+
+  ;; Given the attraction links of A and B, calculate the
+  ;; strength of the TV expressed as
+  ;;
+  ;; min(attraction(X,A), 1 - attraction(X,B))
+  ;;
+  ;; where the attraction(X,A) is the strength of the TV of
+  ;;
+  ;; Attraction <TV>
+  ;;   X
+  ;;   A
+  (define (get-strength A-at B-at)
+    (min (cog-mean A-at) (- 1 (get-pattern-strength B-at))))
+
+  ;; Given the attraction links of A and B, calculate the
+  ;; confidence of the TV expressed as
+  ;;
+  ;; min(attraction(X,A), attraction(X,B))
+  ;;
+  ;; where the attraction(X,A) is the confidence of the TV of
+  ;;
+  ;; Attraction <TV>
+  ;;   X
+  ;;   A
+  (define (get-confidence A-at B-at)
+    (min (cog-confidence A-at) (cog-confidence B-at)))
+
+  (let* ((MembLink conclusion)
+         (A (gar premise))
+         (B (gdr premise))
+         (memb (gar MembLink))
+         (A-at (get-attraction memb A))
+         (B-at (get-attraction memb B))
+         (TVs (get-strength A-at B-at))
+         (TVc (get-confidence A-at B-at))
+         (TV (stv TVs TVc)))
+    (if (< 0 TVc) (cog-merge-hi-conf-tv! MembLink TV))))
+
 ; Name the rule
 (define intensional-difference-members-direct-introduction-rule-name
   (DefinedSchemaNode "intensional-difference-members-direct-introduction-rule"))
