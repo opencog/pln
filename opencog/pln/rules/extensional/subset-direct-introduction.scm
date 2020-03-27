01db3647ec6ee92eef954a735ecdfeb8bb6d61e6
Nil Geisweiller <ngeiswei@gmail.com>
2020-03-27 07:20:52 +0200

    Rename extensional-inheritance to subset direct introduction

--- opencog/pln/rules/extensional/subset-direct-introduction.scm
+++ opencog/pln/rules/extensional/subset-direct-introduction.scm
@@ -1,9 +1,9 @@
-;; Extensional inheritance direct introduction rule
+;; Subset direct introduction rule
 ;;
 ;; A
 ;; B
 ;; |-
-;; ExtensionalInheritance <TV>
+;; Subset <TV>
 ;;   A
 ;;   B
 ;;
@@ -11,7 +11,7 @@
 ;; links (thus is extensional, nor mixed).
 
 ;; Rule
-(define extensional-inheritance-direct-introduction-rule
+(define subset-direct-introduction-rule
   (let* ((A (Variable "$A"))
          (B (Variable "$B"))
          (CT (Type "ConceptNode")))
@@ -23,35 +23,21 @@
         A
         B)
       (ExecutionOutput
-        (GroundedSchema "scm: extensional-inheritance-direct-introduction")
+        (GroundedSchema "scm: subset-direct-introduction")
         (List
           ;; Conclusion
-          (Inheritance A B)
+          (Subset A B)
           ;; Premises
           A
           B)))))
-
-;; Helpers
-(define (variable? X)
-  (or (equal? (cog-type X) 'VariableNode)
-      (equal? (cog-type X) 'GlobNode)))
-
-(define (get-members C)
-"
-  Given a concept node C, return all its members (except variables)
-"
-  (let* ((member-links (cog-filter 'MemberLink (cog-incoming-set C)))
-         (member-of-C? (lambda (x) (equal? C (gdr x))))
-         (members (map gar (filter member-of-C? member-links))))
-    (filter (lambda (x) (not (variable? x))) members)))
 
 ;; Given a list of members of A and B calculate the TV of
 ;;
 ;; ExtensionalInheritance
 ;;   A
 ;;   B
-(define (extensional-inheritance-evidence->tv A-mbrs B-mbrs)
-  (cog-logger-debug "(extensional-inheritance-evidence->tv A-mbrs=~a B-mbrs=~a)" A-mbrs B-mbrs)
+(define (ext-inh-evidence->tv A-mbrs B-mbrs)
+  ;; (cog-logger-debug "(ext-inh-evidence->tv A-mbrs=~a B-mbrs=~a)" A-mbrs B-mbrs)
   (let* ;; TODO consider TVs of the members
        ((A-size (length A-mbrs))
         (AB-mbrs (lset-intersection equal? A-mbrs B-mbrs))
@@ -65,8 +51,8 @@
     (stv strength confidence)))
 
 ;; Formula
-(define (extensional-inheritance-direct-introduction conclusion . premises)
-  ;; (cog-logger-debug "(extensional-inheritance-direct-introduction conclusion=~a . premises=~a)" conclusion premises)
+(define (subset-direct-introduction conclusion . premises)
+  ;; (cog-logger-debug "(subset-direct-introduction conclusion=~a . premises=~a)" conclusion premises)
   (if (= (length premises) 2)
       (let* ((Inh conclusion)
              (A (car premises))
@@ -75,10 +61,10 @@
              (A-mbrs (get-members A))
              (B-mbrs (get-members B))
              ;; Calculate the TV based on the members of A and B
-             (tv (extensional-inheritance-evidence->tv A-mbrs B-mbrs)))
+             (tv (ext-inh-evidence->tv A-mbrs B-mbrs)))
         (cog-merge-hi-conf-tv! Inh tv))))
 
-(define extensional-inheritance-direct-introduction-rule-name
-  (DefinedSchemaNode "extensional-inheritance-direct-introduction-rule"))
-(DefineLink extensional-inheritance-direct-introduction-rule-name
-  extensional-inheritance-direct-introduction-rule)
+(define subset-direct-introduction-rule-name
+  (DefinedSchemaNode "subset-direct-introduction-rule"))
+(DefineLink subset-direct-introduction-rule-name
+  subset-direct-introduction-rule)
