c1a99d30ac6c12352f92706fc612bb8e250d7b30
Anatoly Belikov <awbelikov@gmail.com>
2020-06-16 18:53:37 +0300

    regenerate rules for torchpln

--- opencog/torchpln/pln/rules/propositional/modus-ponens.scm
+++ opencog/torchpln/pln/rules/propositional/modus-ponens.scm
@@ -59,8 +59,9 @@
         (GroundedPredicate "py:pln.rules.propositional.gt_zero_confidence")
         AB)
       ;; Pattern clauses
-      AB
-      A)
+      (Present
+        AB
+        A))
     ;; Rewrite
     (ExecutionOutputLink
       (GroundedSchema "py:pln.rules.propositional.modus_ponens_formula")
@@ -80,10 +81,10 @@
 
 (define (modus-ponens-formula B AB A)
   (let
-      ((sA (cog-stv-strength A))
-       (cA (cog-stv-confidence A))
-       (sAB (cog-stv-strength AB))
-       (cAB (cog-stv-confidence AB))
+      ((sA (cog-mean A))
+       (cA (cog-confidence A))
+       (sAB (cog-mean AB))
+       (cAB (cog-confidence AB))
        (snotAB 0.2)                     ; Huge hack
        (cnotAB 1))
     (cog-set-tv!
