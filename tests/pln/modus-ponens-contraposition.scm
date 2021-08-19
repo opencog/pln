653f3060abe419098e1193a05a1a91295d3df45a
Nil Geisweiller <ngeiswei@gmail.com>
2021-08-19 13:37:05 +0300

    Have PLNUTest use new PLN API

--- tests/pln/modus-ponens-contraposition.scm
+++ tests/pln/modus-ponens-contraposition.scm
@@ -1,19 +1,9 @@
-(use-modules (opencog ure))
+;; Knowledge base for PLNUTest::test_modus_ponens()
 
-
-(define (make-rb rule_base)
-    (MemberLink (DefinedSchemaNode "modus-ponens-inheritance-rule") rule_base)
-    (MemberLink (DefinedSchemaNode "modus-ponens-implication-rule") rule_base)
-    (MemberLink (DefinedSchemaNode "modus-ponens-subset-rule")  rule_base)
-    (MemberLink (DefinedSchemaNode "contraposition-implication-rule") rule_base)
-    (MemberLink (DefinedSchemaNode "contraposition-inheritance-rule") rule_base)
-)
-
-(cog-set-tv! (ConceptNode "apple") (stv 0.8 0.9))
+(ConceptNode "apple" (stv 0.8 0.9))
 (InheritanceLink (stv 0.8 0.4)
-		 (ConceptNode "apple")
+                 (ConceptNode "apple")
                  (ConceptNode "fruit"))
+
 (define rbs (ConceptNode "pln"))
 (define target (ConceptNode "fruit"))
-(make-rb rbs)
-;(cog-bc rbs target)
